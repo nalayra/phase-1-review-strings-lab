7df01b0a42a9ebdf169372e4f7d1bfa2e57d4558
Nicholas Alayra <nalayra@hotmail.com>
2021-11-22 14:46:09 -0800

    commit tree 047309a6b329f7ea8de0f121bc74062ca52981cb

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Nick';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
